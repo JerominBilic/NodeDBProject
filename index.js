2e9be72f9df2e01a6b18a5650fec5cde013b7ede
JerominBilic <85135677+JerominBilic@users.noreply.github.com>
2021-06-23 14:07:02 -0600

    Completed

---
+++ index.js
@@ -0,0 +1,122 @@
+const { urlencoded } = require('express');
+const express = require('express');
+const app = express ();
+const mysql = require('mysql');
+
+app.use(express.urlencoded({extended: false}));
+
+//Creating to connection to our database
+let conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'New',
+    password: '',
+    database: 'microhard'
+});
+
+//Test database connection
+conn.connect(function(err){
+    if(err){
+    console.log(err);
+}else{
+    console.log('Database is connected!')
+}});
+
+app.get('/',function(req,res){
+    let sql = 'SELECT * FROM students';
+    conn.query(sql, function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
+});
+
+//Filter by gender and department
+app.get('/filter',function(req,res){
+    let sql = `SELECT * FROM students where gender = '${req.query.gender}' and department_id = ${req.query.department_id}`
+    conn.query(sql, function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
+});
+
+//Get by gender
+app.get('/gender',function(req,res){
+    let sql = `SELECT * FROM students where gender = '${req.query.gender}'`
+    conn.query(sql, function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
+});
+
+//Get by department
+app.get('/department',function(req,res){
+    let sql = `SELECT * FROM students where department_id = ${req.query.department_id}`
+    conn.query(sql, function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
+});
+
+//Get by ID
+app.get('/id',function(req,res){
+    let sql = `SELECT * FROM students where id = ${req.query.id}`
+    conn.query(sql, function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
+});
+
+//Update
+app.patch('/update',function(req,res){
+    let sql = `UPDATE students set gpa = ${req.query.gpa} where id = ${req.query.id}`
+    conn.query(sql, function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
+});
+
+//Delete
+app.delete('/delete',function(req,res){
+    let sql = `DELETE FROM students where id = ${req.query.id}`
+    conn.query(sql, function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
+});
+    
+
+app.post('/',function(req,res){
+    let sql = `INSERT INTO students(first_name, last_name, age, gender, gpa, department_id) VALUES ('${req.body.first_name}','${req.body.last_name}','${req.body.age}','${req.body.gender}',${req.body.gpa},${req.body.department_id})`;
+
+    conn.query(sql, function(err, result){
+        if(err){
+            res.send(err);
+        }else{
+            res.redirect('/');
+        }
+    })
+});
+
+app.listen(3000, function(){
+    console.log('Server running on port 3000')
+});
